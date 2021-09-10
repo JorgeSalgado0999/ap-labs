57cf46a9ea1bfe50be349fde592ac9cfd563089b
Jorge Salgado <jorgesalgado_isc@outlook.com>
2021-09-10 03:17:40 +0000

    go calculator solved

--- labs/go-calculator/calculator.go
+++ labs/go-calculator/calculator.go
@@ -1,12 +1,76 @@
 package main
 
-// adds/subtracts/multiplies all values that are in the *values array.
-// nValues is the number of values you're reading from the array
-// operator will indicate if it's an addition (1), subtraction (2) or
-// multiplication (3)
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func UNUSED(x ...interface{}) {}
+
 func calc(operator int, values []int) int {
-	return 0
+	sum := 0
+	switch operator {
+	case 1:
+		for i := 0; i < len(values); i++ {
+			sum += int(values[i])
+			if i == len(values)-1 {
+				fmt.Print(values[i], " = ")
+			} else {
+				fmt.Print(values[i], " + ")
+			}
+		}
+		return sum
+
+	case 2:
+		sum += values[0] * 2
+		for i := 0; i < len(values); i++ {
+			sum -= int(values[i])
+			if i == len(values)-1 {
+				fmt.Print(values[i], " = ")
+			} else {
+				fmt.Print(values[i], " - ")
+			}
+		}
+		return sum
+
+	case 3:
+		sum = 1
+		for i := 0; i < len(values); i++ {
+			sum *= int(values[i])
+			if i == len(values)-1 {
+				fmt.Print(values[i], " = ")
+			} else {
+				fmt.Print(values[i], " * ")
+			}
+		}
+		return sum
+	default:
+		return 0
+	}
 }
 
 func main() {
+
+	var vals = make([]int, len(os.Args)-2)
+
+	for i := 2; i < len(os.Args); i++ {
+		num, err := strconv.ParseInt(os.Args[i], 10, 0)
+		vals[i-2] = int(num)
+		UNUSED(err)
+	}
+
+	if os.Args[1] == "add" {
+		fmt.Println(calc(1, vals))
+
+	} else if os.Args[1] == "sub" {
+		fmt.Println(calc(2, vals))
+
+	} else if os.Args[1] == "mult" {
+		fmt.Println(calc(3, vals))
+
+	} else {
+		fmt.Println("invalid input")
+	}
+
 }
